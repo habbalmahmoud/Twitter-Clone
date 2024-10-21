12ca0fb4cf9a662840da73ca5b63cf7d50f9184b
habbalmahmoud <mahhabbal.off1@gmail.cm>
2024-10-21 14:01:58 +0300

    Updated Project Files

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppwriteConstants {
+  static const String databaseId = "671438f6003724bdb154";
+  static const String projectId = "6713a68600105e5c118f";
+  static const String endPoint = 'http://localhost:80/';
+}
